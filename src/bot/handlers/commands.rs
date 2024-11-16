0a3e98650a024b3703ff6580c590442b8f631159
Asqar Arslanov <asqarslanov@gmail.com>
2024-11-17 01:07:42 +0300

    feat: add /start message

--- src/bot/handlers/commands.rs
+++ src/bot/handlers/commands.rs
@@ -1,18 +1,37 @@
+use indoc::{formatdoc, indoc};
 use teloxide::prelude::*;
-// use teloxide::types::ParseMode;
+use teloxide::types::ParseMode;
+use teloxide::utils::markdown;
 
 pub async fn start(bot: Bot, msg: Message) {
+    let snippet = markdown::code_block_with_lang(
+        indoc! {"
+            === Euler's identity:
+            #let exponent = $i pi$
+            $e^exponent + 1 - 0$\
+        "},
+        "typst",
+    );
+    let text = formatdoc! {"
+        Hello! I am @InTypBot!
+        Send me some Typst code and I will render it here.
+
+        For example:
+        {snippet}
+
+        See /help for more details.\
+    "};
+
     let _ = bot
-        .send_message(msg.chat.id, "A greeting message should go here…")
-        // .parse_mode(ParseMode::MarkdownV2)
-        // .disable_notification(true)
+        .parse_mode(ParseMode::MarkdownV2)
+        .send_message(msg.chat.id, text)
         .await;
 }
 
 pub async fn help(bot: Bot, msg: Message) {
     let _ = bot
+        .parse_mode(ParseMode::MarkdownV2)
         .send_message(msg.chat.id, "A help message should go here…")
         // .parse_mode(ParseMode::MarkdownV2)
-        // .disable_notification(true)
         .await;
 }
