e90d1d4b2c1cd602a784a53140f561bb9fa542df
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-30 11:00:42 +0500

    perf: use `formatcp`

--- crates/in-typ-bot/src/config.rs
+++ crates/in-typ-bot/src/config.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use const_format::formatcp;
 use eyre::Context as _;
 use teloxide::types::ChatId;
 
@@ -14,12 +15,12 @@ impl Config {
         const CACHE_CHAT_ID: &str = "CACHE_CHAT_ID";
 
         let teloxide_token =
-            env::var(TELOXIDE_TOKEN).wrap_err_with(|| format!("{TELOXIDE_TOKEN} not found"))?;
+            env::var(TELOXIDE_TOKEN).wrap_err(formatcp!("{TELOXIDE_TOKEN} not found"))?;
         let cache_chat_id = ChatId(
             env::var(CACHE_CHAT_ID)
-                .wrap_err_with(|| format!("{CACHE_CHAT_ID} not found"))?
+                .wrap_err(formatcp!("{CACHE_CHAT_ID} not found"))?
                 .parse::<i64>()
-                .wrap_err_with(|| format!("{CACHE_CHAT_ID} should be an i64"))?,
+                .wrap_err(formatcp!("{CACHE_CHAT_ID} should be an i64"))?,
         );
         Ok(Self {
             teloxide_token,
